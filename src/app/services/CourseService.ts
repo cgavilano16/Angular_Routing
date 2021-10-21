484b2ecbddac656c9810a6ba111e3af19c929c8c
cgavilano16 <cesar.gavilano@student.fairfield.edu>
2021-10-21 19:27:48 -0400

    changes to show objects on browswer

--- src/app/services/CourseService.ts
+++ src/app/services/CourseService.ts
@@ -3,6 +3,6 @@ import { Injectable} from "@angular/core";
 @Injectable()
 export class CourseService {
     findAllCourses = () =>
-        fetch('http://localhost:8089/api/courses', {mode: 'no-cors'})
+        fetch('http://localhost:8089/api/courses')
         .then(response => response.json())
 }
