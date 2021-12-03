4691174f0b4d2baca8d1ac2afc1bb2d4b3aa4b7a
cgavilano16 <cesar.gavilano@student.fairfield.edu>
2021-12-02 19:43:27 -0500

    Cross-Origin error fix

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var cors = require('cors');
 const app = express();
+app.use(cors())
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angular-routing'));
 app.get('/*', function(req,res) {    
