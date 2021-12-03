5a67c4f4298fd96ea81739fa65d9f353dbd37d8d
cgavilano16 <cesar.gavilano@student.fairfield.edu>
2021-12-02 19:37:23 -0500

    new name updated

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Angular_Routing'));
+app.use(express.static(__dirname + '/dist/angular-routing'));
 app.get('/*', function(req,res) {    
-res.sendFile(path.join(__dirname+'/dist/Angular_Routing/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-routing/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
